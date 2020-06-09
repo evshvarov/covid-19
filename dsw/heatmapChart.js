82b61c83658d657f19de2e78bd89f9fa7d8d7f66
gnibeda <anton.donnu@gmail.com>
2020-06-09 13:36:07 +0300

    added new version of heatmap addon

--- dsw/heatmapChart.js
+++ dsw/heatmapChart.js
@@ -1,5 +1,14 @@
 (function () {
     'use strict';
+
+    /**
+     * Example Data property
+     *
+     * {"padding": 1, "borderColor": "#FFFFFF", "minColor": "rgba(0,0,255,0.51)", "maxColor": "rgba(82,255,28,0.6)", "stops": [["", "#000000"], [2, "#00FF00"], [14, "#0000FF"], [15, "#FF0000"]]}
+     *
+     */
+
+    // Mock data for testing
     // const d = {
     //     "Info": {"Error": "", "KpiName": "SleepRecord.kpi"}, "Result": {
     //         "Properties": [{"name": 1, "caption": 12, "columnNo": 1}, {
@@ -220,7 +229,6 @@
     //         }]
     //     }
     // };
-
     // const d = {"Info":{"Error":"","KpiName":"SleepRecord.kpi"},"Result":{"Properties":[{"name":1,"caption":12,"columnNo":1},{"name":2,"caption":13,"columnNo":2},{"name":3,"caption":14,"columnNo":3},{"name":4,"caption":15,"columnNo":4},{"name":5,"caption":16,"columnNo":5},{"name":6,"caption":17,"columnNo":6},{"name":7,"caption":18,"columnNo":7},{"name":8,"caption":19,"columnNo":8},{"name":9,"caption":20,"columnNo":9},{"name":10,"caption":21,"columnNo":10},{"name":11,"caption":22,"columnNo":11},{"name":12,"caption":23,"columnNo":12},{"name":13,"caption":24,"columnNo":13},{"name":14,"caption":1,"columnNo":14},{"name":15,"caption":2,"columnNo":15},{"name":16,"caption":3,"columnNo":16},{"name":17,"caption":4,"columnNo":17},{"name":18,"caption":5,"columnNo":18},{"name":19,"caption":6,"columnNo":19},{"name":20,"caption":7,"columnNo":20},{"name":21,"caption":8,"columnNo":21},{"name":22,"caption":9,"columnNo":22},{"name":23,"caption":10,"columnNo":23},{"name":24,"caption":11,"columnNo":24}],"Series":[{"1":"","2":"","3":"","4":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 120 mins\"}","5":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 120 mins\"}","6":"","7":"","8":"","9":"","10":"","11":"","12":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","13":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","14":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","15":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","16":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","17":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","18":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","19":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","20":"{\"value:\".75,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","21":"","22":"","23":"","24":"","%series":"01/06/2020"},{"1":"","2":"","3":"","4":"","5":"","6":"","7":"","8":"","9":"","10":"","11":"","12":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","13":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","14":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","15":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","16":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","17":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","18":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","19":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","20":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","21":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 600 mins\"}","22":"","23":"","24":"","%series":"02/06/2020"},{"1":"","2":"","3":"","4":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 135 mins\"}","5":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 135 mins\"}","6":"{\"value:\".25,\"tooltip\":\"Event: Sleep\\nDuration: 135 mins\"}","7":"","8":"","9":"","10":"","11":"","12":"","13":"","14":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","15":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","16":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","17":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","18":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","19":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","20":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","21":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","22":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","23":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","24":"{\"value:\".25,\"tooltip\":\"Event: Sleep\\nDuration: 615 mins\"}","%series":"03/06/2020"},{"1":"","2":"","3":"","4":"","5":"","6":"","7":"","8":"","9":"","10":"","11":"","12":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","13":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","14":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","15":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","16":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","17":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","18":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","19":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","20":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 540 mins\"}","21":"","22":"","23":"","24":"","%series":"04/06/2020"},{"1":"","2":"","3":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 105 mins\"}","4":"{\"value:\".75,\"tooltip\":\"Event: Sleep\\nDuration: 105 mins\"}","5":"","6":"","7":"","8":"","9":"","10":"","11":"","12":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","13":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","14":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","15":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","16":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","17":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","18":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","19":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","20":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","21":"{\"value:\".75,\"tooltip\":\"Event: Sleep\\nDuration: 585 mins\"}","22":"","23":"","24":"","%series":"05/06/2020"},{"1":"{\"value:\".75,\"tooltip\":\"Event: Sleep\\nDuration: 45 mins\"}","2":"","3":"","4":"","5":"","6":"","7":"","8":"","9":"","10":"","11":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","12":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","13":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","14":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","15":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","16":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","17":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","18":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","19":"{\"value:\".75,\"tooltip\":\"Event: Sleep\\nDuration: 525 mins\"}","20":"","21":"","22":"","23":"","24":"","%series":"06/06/2020"},{"1":"","2":"","3":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 105 mins\"}","4":"{\"value:\".75,\"tooltip\":\"Event: Sleep\\nDuration: 105 mins\"}","5":"","6":"","7":"","8":"","9":"","10":"","11":"","12":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","13":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","14":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","15":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","16":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","17":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","18":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","19":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","20":"{\"value\":1,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","21":"{\"value:\".5,\"tooltip\":\"Event: Sleep\\nDuration: 570 mins\"}","22":"","23":"","24":"","%series":"07/06/2020"}]}};
 
     function HeatmapChartFact(BaseChart, Utils) {
@@ -238,10 +246,23 @@
             $scope.item.toggleValues = toggleValues;
 
             var padding = 4;
-            if ($scope.item.properties) {
-                var p = +$scope.item.properties.Padding;
-                if (p !== undefined && p !== null && !isNaN(p)) {
-                    padding = p;
+            var minColor = 'rgba(0,0,255,0.51)';
+            var maxColor = 'rgba(255,82,28,0.6)';
+            var borderColor = '#FFFFFF';
+            var stops = [];
+            if (_this.desc.properties) {
+                var userConfData = _this.desc.properties.Data;
+                if (userConfData) {
+                    try {
+                        var obj = JSON.parse(userConfData);
+                        if (obj.padding !== undefined) padding = obj.padding;
+                        if (obj.minColor !== undefined) minColor = obj.minColor;
+                        if (obj.maxColor !== undefined) maxColor = obj.maxColor;
+                        if (obj.borderColor !== undefined) borderColor = obj.borderColor;
+                        if (obj.stops !== undefined) stops = obj.stops;
+                    } catch (e) {
+                        console.warn('Invalid data for heatmap: ', userConfData);
+                    }
                 }
             }
 
@@ -253,7 +274,7 @@
                 plotOptions: {
                     heatmap: {
                         borderWidth: padding,
-                        borderColor: '#FFFFFF',
+                        borderColor: borderColor,
                         dataLabels: {
                             format: '{point.value}',
                             enabled: $scope.item.showValues,
@@ -267,8 +288,8 @@
                     }
                 },
                 colorAxis: {
-                    minColor: 'rgba(0,0,255,0.51)',
-                    maxColor: 'rgba(255,82,28,0.6)'
+                    minColor: minColor,
+                    maxColor: maxColor
                 }
             };
 
@@ -292,8 +313,7 @@
 
             this.requestData();
 
-
-            // this._retriveKPI(d);
+            //this._retriveKPI(d);
 
             function toggleValues() {
                 _this.toggleButton("showValues");
@@ -312,13 +332,13 @@
                 $scope.chartConfig.series = [];
                 $scope.chartConfig.xAxis.categories = [];
                 for (i = 0; i < data.Cols[0].tuples.length; i++) {
-                    $scope.chartConfig.xAxis.categories.push(data.Cols[0].tuples[i].caption.toString());
+                    $scope.chartConfig.xAxis.categories.push((data.Cols[0].tuples[i].caption || '').toString());
                 }
 
                 $scope.chartConfig.series = [];
                 $scope.chartConfig.yAxis.categories = [];
                 for (i = data.Cols[1].tuples.length - 1; i >= 0; i--) {
-                    $scope.chartConfig.yAxis.categories.push(data.Cols[1].tuples[i].caption.toString());
+                    $scope.chartConfig.yAxis.categories.push((data.Cols[1].tuples[i].caption || '').toString());
                 }
 
 
@@ -335,6 +355,11 @@
 
                         //tempData.push(row);
                         tempData.push({x: j, y: i, value: v.value, tooltipData: v.tooltip});
+
+                        var stop = stops.find(s => s[0] === v.value);
+                        if (stop) {
+                            tempData[tempData.length - 1].color = stop[1];
+                        }
                     }
                 }
                 _this.addSeries({
